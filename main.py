d212320b5c4b9c392159d8e468736548349e05b9
MaGo1024 <olumideodusote@gmail.com>
2026-02-06 02:27:36 +0000

    Create a Snake Class & Move to OOP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Turtle, Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,24 +11,16 @@ screen.tracer(0)
 starting_position = [(0,0), (-20, 0), (-40, 0)]
 segments = []
 
-for position in starting_position:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+# create snake
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    # move each segment to the position of the previous segment
-    for seg_num in range(len(segments)-1,0,-1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
+    # move snake
+    snake.move()
+
 
 
 
